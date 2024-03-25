56a9337821fd5768a5b7a30a442ce23eecd2abae
Oscar Mancilla <oscarmancilla227@gmail.com>
2024-03-25 11:09:35 -0600

    Se termino el modulo de busquedas

--- controller/buscarController.js
+++ controller/buscarController.js
@@ -1,6 +1,8 @@
 import { Types } from 'mongoose';
 const { ObjectId } = Types;
 import { UserModel } from "../model/usuario.js";
+import { CategoriaModel } from '../model/categoria.js';
+import { ProductoModel } from '../model/producto.js';
 
 const colecciones = ['usuarios', 'categorias', 'productos', 'roles'];
 
@@ -8,12 +10,7 @@ const buscarUsuarios = async (termino, res) => {
     try {
         const isMongo = ObjectId.isValid(termino);
 
-        if (!isMongo) {
-            return res.json({
-                msg: "El ID proporcionado no es válido."
-            });
-        }
-
+        if (isMongo) {
         const user = await UserModel.findById(termino);
 
         if (user) {
@@ -21,6 +18,20 @@ const buscarUsuarios = async (termino, res) => {
         } else {
             return res.json({ results: [] });
         }
+        }
+
+        
+        const regex = new RegExp(termino, 'i');
+        
+        const usuarios = await UserModel.find({
+            $or: [{$and: [{nombre: regex}, {state: true}]}, {$and: [{correo: regex}, {state: true}]}]
+        });
+
+        return res.json({
+            results: [usuarios]
+        })
+
+       
     } catch (error) {
         console.error("Error al buscar usuarios:", error);
         return res.status(500).json({
@@ -29,8 +40,75 @@ const buscarUsuarios = async (termino, res) => {
     }
 };
 
+const buscarCategorias = async(termino, res) => {
+    try {
+        const isMongo = ObjectId.isValid(termino);
+
+        if (isMongo) {
+        const categoria = await CategoriaModel.findById(termino);
+
+        if (categoria) {
+            return res.json({ results: [categoria] });
+        } else {
+            return res.json({ results: [] });
+        }
+        }
+
+        const regex = new RegExp(termino, 'i');
+
+        const categorias = await CategoriaModel.find({
+            $and: [{nombre: regex}, {state: true}]
+        });
+
+        return res.json({
+            results: [categorias]
+        })
+
+       
+    } catch (error) {
+        console.error("Error al buscar usuarios:", error);
+        return res.status(500).json({
+            msg: "Hubo un error"
+        });
+    }
+}
+
+const buscarProductos = async(termino, res) => {
+    try {
+        const isMongo = ObjectId.isValid(termino);
+
+        if (isMongo) {
+        const producto = await ProductoModel.findById(termino);
+
+        if (producto) {
+            return res.json({ results: [producto] });
+        } else {
+            return res.json({ results: [] });
+        }
+        }
+
+        const regex = new RegExp(termino, 'i');
+
+        const productos = await ProductoModel.find({
+            $or: [{$and: [{nombre: regex}, {state: true}]}]
+        });
+
+        return res.json({
+            results: [productos]
+        })
+
+       
+    } catch (error) {
+        console.error("Error al buscar usuarios:", error);
+        return res.status(500).json({
+            msg: "Hubo un error."
+        });
+    }
+}
+
 const buscar = async (req, res) => {
-    const { coleccion, termino } = req.params;
+    const {coleccion} = req.params;
+    const {termino} = req.params;
 
     if (!colecciones.includes(coleccion)) {
         return res.status(401).json({
@@ -40,15 +118,19 @@ const buscar = async (req, res) => {
 
     switch (coleccion) {
         case 'usuarios':
-            await buscarUsuarios(termino, res);
+                await buscarUsuarios(termino, res);
+
             break;
 
         case 'categorias':
-            // Lógica para buscar categorías
+
+                await buscarCategorias(termino, res);
+
             break;
 
         case 'productos':
-            // Lógica para buscar productos
+            
+                await buscarProductos(termino, res)
             break;
 
         case 'roles':
